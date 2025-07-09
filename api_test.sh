81cd2aa3a56b66c72a654e019d592318ea93de1e
Alex CAastanea <jhon.castaneda.angulo@gmail.com>
2025-07-09 15:28:34 +0200

    add files

--- api_test.sh
+++ api_test.sh
@@ -27,18 +27,23 @@ MOVIE_PAYLOAD=$(jq -n \
 MOVIE_RESPONSE=$(curl -s -X POST "$BASE_URL/movies/" \
   -H "Content-Type: application/json" \
   -d "$MOVIE_PAYLOAD")
+echo "POST /movies/ response:"
 echo "$MOVIE_RESPONSE" | jq
 MOVIE_ID=$(echo "$MOVIE_RESPONSE" | jq '.id')
 echo "Created movie with ID: $MOVIE_ID"
 echo
 
 echo "3. List all movies"
-curl -s -X GET "$BASE_URL/movies/" | jq
+MOVIES_LIST=$(curl -s -X GET "$BASE_URL/movies/")
+echo "GET /movies/ response:"
+echo "$MOVIES_LIST" | jq
 echo
 
 if [ -n "$MOVIE_ID" ] && [ "$MOVIE_ID" != "null" ]; then
   echo "4. Get the movie by ID"
-  curl -s -X GET "$BASE_URL/movies/$MOVIE_ID" | jq
+  MOVIE_BY_ID=$(curl -s -X GET "$BASE_URL/movies/$MOVIE_ID")
+  echo "GET /movies/$MOVIE_ID response:"
+  echo "$MOVIE_BY_ID" | jq
   echo
 
   echo "5. Update the movie"
@@ -48,13 +53,17 @@ if [ -n "$MOVIE_ID" ] && [ "$MOVIE_ID" != "null" ]; then
     --argjson genres '["Sci-Fi", "Action"]' \
     --argjson casts_id "[$CAST_ID1, $CAST_ID2]" \
     '{name: $name, plot: $plot, genres: $genres, casts_id: $casts_id}')
-  curl -s -X PUT "$BASE_URL/movies/$MOVIE_ID" \
+  UPDATE_RESPONSE=$(curl -s -X PUT "$BASE_URL/movies/$MOVIE_ID" \
     -H "Content-Type: application/json" \
-    -d "$UPDATE_PAYLOAD" | jq
+    -d "$UPDATE_PAYLOAD")
+  echo "PUT /movies/$MOVIE_ID response:"
+  echo "$UPDATE_RESPONSE" | jq
   echo
 
   echo "6. Delete the movie"
-  curl -s -X DELETE "$BASE_URL/movies/$MOVIE_ID" | jq
+  DELETE_RESPONSE=$(curl -s -X DELETE "$BASE_URL/movies/$MOVIE_ID")
+  echo "DELETE /movies/$MOVIE_ID response:"
+  echo "$DELETE_RESPONSE" | jq
   echo
 else
   echo "Movie ID not found, skipping get/update/delete movie steps."
@@ -67,9 +76,11 @@ MOVIE_PAYLOAD2=$(jq -n \
   --argjson genres '["Sci-Fi"]' \
   --argjson casts_id "[$CAST_ID1]" \
   '{name: $name, plot: $plot, genres: $genres, casts_id: $casts_id}')
-curl -s -X POST "$BASE_URL/movies/" \
+MOVIE_RESPONSE2=$(curl -s -X POST "$BASE_URL/movies/" \
   -H "Content-Type: application/json" \
-  -d "$MOVIE_PAYLOAD2" | jq
+  -d "$MOVIE_PAYLOAD2")
+echo "POST /movies/ (Interstellar) response:"
+echo "$MOVIE_RESPONSE2" | jq
 echo
 
 echo "8. Try to create a movie with a non-existent cast ID"
@@ -79,9 +90,11 @@ BAD_MOVIE_PAYLOAD=$(jq -n \
   --argjson genres '["Horror"]' \
   --argjson casts_id '[9999]' \
   '{name: $name, plot: $plot, genres: $genres, casts_id: $casts_id}')
-curl -s -X POST "$BASE_URL/movies/" \
+BAD_MOVIE_RESPONSE=$(curl -s -X POST "$BASE_URL/movies/" \
   -H "Content-Type: application/json" \
-  -d "$BAD_MOVIE_PAYLOAD" | jq
+  -d "$BAD_MOVIE_PAYLOAD")
+echo "POST /movies/ (bad cast) response:"
+echo "$BAD_MOVIE_RESPONSE" | jq
 echo
 
 echo "=== Done! ==="
